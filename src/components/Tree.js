cbb524b6d6e5789036e3b11dcafe89b2d35418a7
Yash Jindal <yashjindal069@gmail.com>
2023-05-07 19:21:03 +0530

    changes done

--- src/components/Tree.js
+++ src/components/Tree.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Node from "./Node";
 import "./style.css";
 
@@ -16,13 +16,16 @@ const Tree = ({ treeArray, onNodeClick }) => {
     onNodeClick(index);
   };
 
+  useEffect(() => {
+    setHighlightedNodes([]);
+  }, [treeArray]);
+
   const getNodesAtLevel = (level) => {
     // console.log(level);
     const startIndex = 2 ** level - 1;
     const endIndex = Math.min(2 ** (level + 1) - 2, treeArray.length - 1);
     return treeArray.slice(startIndex, endIndex + 1);
   };
-
   const maxLevel = Math.ceil(Math.log2(treeArray.length) + 1);
   const levels = Array.from({ length: maxLevel }, (_, i) => i);
   const nodesByLevel = levels.map((level) => getNodesAtLevel(level));
